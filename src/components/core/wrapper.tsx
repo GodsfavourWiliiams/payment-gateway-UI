d4b5aca42788d4be214a2dd42457fff2d8b5843d
Godsfavour Williams <godzfeivvur@gmail.com>
2023-05-25 14:28:08 +0100

    feat: added background img

--- src/components/core/wrapper.tsx
+++ src/components/core/wrapper.tsx
@@ -6,7 +6,7 @@ type Props = {
 
 const Wrapper = (props: Props) => {
     return (
-        <div className='bg-indigo-300 p-4 sm:p-8 absolute top-0 left-0 right-0 bottom-0 flex items-center md:h-full h-fit overflow-auto'>
+        <div className='bg-windowsImgLg lg:bg-windowsImgXl lg:bg-left bg-no-repeat p-4 sm:p-8 absolute top-0 left-0 right-0 bottom-0 flex items-center md:h-full h-fit overflow-auto'>
             <div className="w-full max-w-6xl m-auto p-6 bg-[#FBFCFE] h-fit relative ">
                 {props.children}
             </div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,13 @@
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      backgroundImage: {
+        windowsImgXl: "url(../src/assets/3280.jpg)",
+        windowsImgLg: "url(../src/assets/1125.jpg)",
+        windowsImgSm: "url(../src/assets/520.jpg)",
+      },
+    },
   },
   plugins: [],
 };
